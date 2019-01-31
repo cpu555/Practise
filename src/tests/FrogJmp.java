e17c9ca3b9b5c64b1bf102080a6f5f5e9471d59d
Morhaf <morhaf.ko91@gmail.com>
2019-02-01 00:27:10 +0100

    Copy write added

--- src/tests/FrogJmp.java
+++ src/tests/FrogJmp.java
@@ -12,7 +12,6 @@ public class FrogJmp {
         else
             return (int) Math.ceil((double)(Y-X)/D);
     }
-
     public static void main(String[] args) {
         System.out.println(solution(10,85,30));
     }
